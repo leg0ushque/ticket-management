90e9eaf4888a72d4a65f5336e4e43f31c1960b5f
Vadzim Zemchonak <vadzim_zemchonak@epam.com>
2024-05-15 00:50:55 +0300

    Update answers, update DAL test coverage

---
+++ tests/TicketingSystem.DataAccess.Tests/Repositories/UserRepositoryTests.cs
@@ -0,0 +1,17 @@
+﻿using TicketingSystem.DataAccess.Entities;
+using TicketingSystem.DataAccess.Factories;
+using TicketingSystem.DataAccess.Repositories;
+
+namespace TicketingSystem.UnitTests
+{
+    public class UserRepositoryTests : GenericMongoRepositoryTestsBase<User>
+    {
+        public UserRepositoryTests()
+        {
+            Setup();
+        }
+
+        public override IMongoRepository<User> CreateRepository(IMongoDbFactory factory)
+            => new UserRepository(factory);
+    }
+}
