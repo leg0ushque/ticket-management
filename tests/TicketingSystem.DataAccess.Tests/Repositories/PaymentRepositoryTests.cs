927054ceb250fea79b26df6032dd793d097768e2
Vadzim Zemchonak <vadzim_zemchonak@epam.com>
2024-05-15 00:52:35 +0300

    Revert "revert changes not related to previous home task's PR"
    This reverts commit a6966994ad43f5edc43b26d6ebbaecc758353b89.

---
+++ tests/TicketingSystem.DataAccess.Tests/Repositories/PaymentRepositoryTests.cs
@@ -0,0 +1,17 @@
+﻿using TicketingSystem.DataAccess.Entities;
+using TicketingSystem.DataAccess.Factories;
+using TicketingSystem.DataAccess.Repositories;
+
+namespace TicketingSystem.UnitTests
+{
+    public class PaymentRepositoryTests : GenericMongoRepositoryTestsBase<Payment>
+    {
+        public PaymentRepositoryTests()
+        {
+            Setup();
+        }
+
+        public override IMongoRepository<Payment> CreateRepository(IMongoDbFactory factory)
+            => new PaymentRepository(factory);
+    }
+}
---
+++ tests/TicketingSystem.DataAccess.Tests/Repositories/SectionRepositoryTests.cs
@@ -0,0 +1,17 @@
+﻿using TicketingSystem.DataAccess.Entities;
+using TicketingSystem.DataAccess.Factories;
+using TicketingSystem.DataAccess.Repositories;
+
+namespace TicketingSystem.UnitTests
+{
+    public class SectionRepositoryTests : GenericMongoRepositoryTestsBase<Section>
+    {
+        public SectionRepositoryTests()
+        {
+            Setup();
+        }
+
+        public override IMongoRepository<Section> CreateRepository(IMongoDbFactory factory)
+            => new SectionRepository(factory);
+    }
+}
